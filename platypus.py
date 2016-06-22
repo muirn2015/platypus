ef1efd413d8eb77cb395e20ee288754a8afe6865
Neil Muir <muirn2015@gmail.com>
2016-06-22 14:59:45 -0400

    Add files via upload

--- platypus.py
+++ platypus.py
@@ -38,9 +38,33 @@ class Line(Entity):
 
 
 def residual(a,b):
-    if a.type == 1 and b.type == 1:
-        print "point to point"
-        print "residual = ",np.sqrt(np.sum(np.square(a.xyz-b.xyz)))
-    elif a.type == 1 and b.type == 2 or a.type == 2 and b.type == 1:
-        print "point to point"
-        print "residual = ?"
+	if a.type == 1 and b.type == 1:
+		print "point to point"
+        	print "residual = ",np.sqrt(np.sum(np.square(a.xyz-b.xyz)))
+	elif a.type == 1 and b.type == 2:
+        	print "point to line"
+        	vecu= np.subtract(a.xyz-b.xyz)
+		vecv= np.subtract(b.xyz-b.vector)
+		magu = np.sqrt(np.sum(np.square(vecu)))
+		magv = np.sqrt(np.sum(np.square(vecv)))
+		cos0=np.sum(np.multiply(vecu,vecv))/(magv*magu)
+		if (cos0<=0):
+			print "residual=", magu
+		else:
+			coslen=cos0*magu
+			dist=np.sqrt(np.sum(np.square(magu)-np.square(coslen)))
+			print "residual=", dist
+	
+	elif a.type == 2 and b.type == 1:
+        	print "point to line"
+        	vecu= np.subtract(b.xyz-a.xyz)
+		vecv= np.subtract(a.xyz-a.vector)
+		magu = np.sqrt(np.sum(np.square(vecu)))
+		magv = np.sqrt(np.sum(np.square(vecv)))
+		cos0=np.sum(np.multiply(vecu,vecv))/(magv*magu)
+		if (cos0<=0):
+			print "residual=", magu
+		else:
+			coslen=cos0*magu
+			dist=np.sqrt(np.sum(np.square(magu)-np.square(coslen)))
+			print "residual=", dist
